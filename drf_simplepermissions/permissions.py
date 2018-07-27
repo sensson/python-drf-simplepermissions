a7df63ee2f618e5e980e4667f02bf9fc93f143f3
ju5t <m@ju5t.nl>
2018-07-27 14:40:04 +0200

    Python3 compatibility

--- drf_simplepermissions/permissions.py
+++ drf_simplepermissions/permissions.py
@@ -3,6 +3,11 @@ from rest_framework import permissions
 
 class SimplePermissions(permissions.BasePermission):
     def has_permission(self, request, view):
+        try:
+            basestring
+        except NameError:
+            basestring = str
+
         if isinstance(view.allowed_permissions, (list, tuple)):
             for permission in view.allowed_permissions:
                 if request.user.has_perm(permission):
