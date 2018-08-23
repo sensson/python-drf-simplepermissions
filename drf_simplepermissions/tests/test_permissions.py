7f6fc3114a3f1dc272b224732eb924574c1dcee3
ju5t <m@ju5t.nl>
2018-08-23 15:37:30 +0200

    (#1) Improve tests
    Use an actual user to test our permissions. The View and Request
    can be kept as is as we don't use them to test a permission.

--- drf_simplepermissions/tests/test_permissions.py
+++ drf_simplepermissions/tests/test_permissions.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 from drf_simplepermissions import SimplePermissions
 
 
@@ -11,47 +14,53 @@ class Request:
         self.user = None
 
 
-class User:
-    def __init__(self, permissions):
-        self.permissions = permissions
-
-    def has_perm(self, permission):
-        if permission in self.permissions:
-            return True
-
-
 class TestPermissions(TestCase):
     def setUp(self):
         self.permissions = SimplePermissions()
         self.request = Request()
         self.view = View()
 
+        # Create a user
+        user = User.objects.create_user(username='demo', password='demo')
+        user.save()
+        self.request.user = user
+
+        # Create a content_type to test with
+        self.content_type = ContentType(app_label='test_case', model='permissions') # noqa
+        self.content_type.save()
+
     def test_basic_tuple_permission(self):
-        self.view.allowed_permissions = ('test',)
-        self.request.user = User(permissions=('test',))
+        permission = Permission.objects.create(codename='test', content_type=self.content_type) # noqa
+        self.request.user.user_permissions.add(permission)
+        self.view.allowed_permissions = ('test_case.test',)
         self.assertEqual(self.permissions.has_permission(request=self.request, view=self.view), True) # noqa
 
     def test_incorrect_tuple_permission(self):
-        self.view.allowed_permissions = ('test',)
-        self.request.user = User(permissions=('test2',))
+        permission = Permission.objects.create(codename='test2', content_type=self.content_type) # noqa
+        self.request.user.user_permissions.add(permission)
+        self.view.allowed_permissions = ('test_case.test',)
         self.assertEqual(self.permissions.has_permission(request=self.request, view=self.view), False) # noqa
 
     def test_basic_list_permission(self):
-        self.view.allowed_permissions = ['test', ]
-        self.request.user = User(permissions=('test',))
+        permission = Permission.objects.create(codename='test', content_type=self.content_type) # noqa
+        self.request.user.user_permissions.add(permission)
+        self.view.allowed_permissions = ['test_case.test', ]
         self.assertEqual(self.permissions.has_permission(request=self.request, view=self.view), True) # noqa
 
     def test_incorrect_list_permission(self):
-        self.view.allowed_permissions = ['test', ]
-        self.request.user = User(permissions=('test2',))
+        permission = Permission.objects.create(codename='test2', content_type=self.content_type) # noqa
+        self.request.user.user_permissions.add(permission)
+        self.view.allowed_permissions = ['test_case.test', ]
         self.assertEqual(self.permissions.has_permission(request=self.request, view=self.view), False) # noqa
 
     def test_basic_string_permission(self):
-        self.view.allowed_permissions = 'test'
-        self.request.user = User(permissions=('test',))
+        permission = Permission.objects.create(codename='test', content_type=self.content_type) # noqa
+        self.request.user.user_permissions.add(permission)
+        self.view.allowed_permissions = 'test_case.test'
         self.assertEqual(self.permissions.has_permission(request=self.request, view=self.view), True) # noqa
 
     def test_incorrect_string_permission(self):
+        permission = Permission.objects.create(codename='test2', content_type=self.content_type) # noqa
+        self.request.user.user_permissions.add(permission)
         self.view.allowed_permissions = 'test'
-        self.request.user = User(permissions=('test2',))
         self.assertEqual(self.permissions.has_permission(request=self.request, view=self.view), False) # noqa
