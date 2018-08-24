c5b03c4a8ce6fcebaa9e9826284cd158e6881caa
ju5t <m@ju5t.nl>
2018-08-24 21:39:18 +0200

    Drop demo_group as an attribute of is_demo()
    This would lead to a lot of duplicate code. Instead we now check
    settings.DEMO_GROUPS for a list of valid demo groups.

--- drf_simplepermissions/permissions.py
+++ drf_simplepermissions/permissions.py
@@ -23,10 +23,10 @@ class SimplePermissions(permissions.BasePermission):
         return False
 
 
-def is_demo(user, demo_group='demo'):
-    '''is_demo checks if a user is added to a demo group or groups. If a
-    list of groups is supplied, it will try to match against any group and
-    if a match is found, it will return true.
+def is_demo(user):
+    '''is_demo checks if a user is added to a demo group or groups. It will try
+    to match against any group and if a match is found, it will return true. It
+    uses `settings.DEMO_GROUPS` as its source to check against.
 
     Global demo mode can be enabled by changing `settings.DEMO` to True. If
     it's set to False you can override it by adding users to a group. You
@@ -41,6 +41,8 @@ def is_demo(user, demo_group='demo'):
     except NameError:
         basestring = str
 
+    demo_groups = 'demo'
+
     if hasattr(settings, 'DEMO'):
         if not isinstance(settings.DEMO, bool):
             raise SimpleModeException('DEMO {0} is unsupported'.format(
@@ -50,11 +52,14 @@ def is_demo(user, demo_group='demo'):
         if settings.DEMO:
             return settings.DEMO
 
-    if isinstance(user, User):
-        if isinstance(demo_group, basestring):
-            return user.groups.filter(name=demo_group).exists()
+    if hasattr(settings, 'DEMO_GROUPS'):
+        demo_groups = settings.DEMO_GROUPS
 
-        if isinstance(demo_group, (list)):
-            return user.groups.filter(name__in=demo_group).exists()
+    if isinstance(user, User):
+        if isinstance(demo_groups, basestring):
+            return user.groups.filter(name=demo_groups).exists()
+
+        if isinstance(demo_groups, (list)):
+            return user.groups.filter(name__in=demo_groups).exists()
 
     return False
