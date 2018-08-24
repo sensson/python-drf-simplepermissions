ffbdce86ceac2ed6b27c7c1f5d05ddcc2c92c1d2
ju5t <m@ju5t.nl>
2018-08-24 08:35:47 +0200

    Drop demo_mode as an attribute of is_demo()
    This would lead to a lot of duplicate code. Instead we now check if
    settings.DEMO is True and if so, we'll enable demo mode. If it's
    False, you can override it by adding the user to a predefined
    group.

--- drf_simplepermissions/permissions.py
+++ drf_simplepermissions/permissions.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from drf_simplepermissions.exceptions import SimpleModeException
@@ -22,27 +23,32 @@ class SimplePermissions(permissions.BasePermission):
         return False
 
 
-def is_demo(user, demo_group='demo', demo_mode=False):
+def is_demo(user, demo_group='demo'):
     '''is_demo checks if a user is added to a demo group or groups. If a
     list of groups is supplied, it will try to match against any group and
     if a match is found, it will return true.
 
+    Global demo mode can be enabled by changing `settings.DEMO` to True. If
+    it's set to False you can override it by adding users to a group. You
+    can't override `settings.DEMO` when it's set to True.
+
     This allows you to setup demo accounts that are restricted in what they
     can do or what they can see. Keep in mind that it is not intended to
-    be used as a permission_classes' class.'''
+    be used as a permission_classes' class as that has a different purpose.'''
 
     try:
         basestring
     except NameError:
         basestring = str
 
-    if demo_mode is not False:
-        if not isinstance(demo_mode, bool):
-            raise SimpleModeException('demo_mode {0} is unsupported'.format(
-                demo_mode,
+    if hasattr(settings, 'DEMO'):
+        if not isinstance(settings.DEMO, bool):
+            raise SimpleModeException('DEMO {0} is unsupported'.format(
+                settings.DEMO,
             ))
 
-        return demo_mode
+        if settings.DEMO:
+            return settings.DEMO
 
     if isinstance(user, User):
         if isinstance(demo_group, basestring):
