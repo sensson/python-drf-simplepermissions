7da4f968ac280ca471268f7608a844c105bca5eb
ju5t <m@ju5t.nl>
2020-12-09 17:06:13 +0100

    chore: setup new repository details

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='drf-simplepermissions',
       version='0.0.4',
       description='SimplePermissions checks against a list of permissions',
-      url='https://git.sensson.net/bellmann/python-drf-simplepermissions',
+      url='https://github.com/bellmann/python-drf-simplepermissions',
       author='Bellmann BV',
-      author_email='ton@bellmann.nl',
+      author_email='opensource@bellmann.nl',
       packages=['drf_simplepermissions'],
       install_requires=[
           'django',
